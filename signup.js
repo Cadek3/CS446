65713a3884e21bb4ab5dc7d9bcd33852b99f3ec6
Kinsey <kinseycade@gmail.com>
2023-11-15 19:36:44 -0800

    Home page Update

---
+++ signup.js
@@ -0,0 +1,9 @@
+// popup.js
+
+document.getElementById('openPopup').addEventListener('click', function () {
+    document.getElementById('signupPopup').style.display = 'block';
+});
+
+document.getElementById('closePopup').addEventListener('click', function () {
+    document.getElementById('signupPopup').style.display = 'none';
+});
