2c4ee61889d7483e44abf4ae2f9f226f5b490649
Kinsey <kinseycade@gmail.com>
2023-11-16 18:54:53 -0800

    overlay modified

--- signup.js
+++ signup.js
@@ -1,14 +1,26 @@
+// signup.js
+
 document.getElementById('openPopup').addEventListener('click', function () {
     document.getElementById('signupPopup').style.display = 'block';
+    on();
 });
 
 document.getElementById('closePopup').addEventListener('click', function () {
     document.getElementById('signupPopup').style.display = 'none';
+    off();
 });
 
 function on() {
     document.getElementById("overlay").style.display = "block";
 }
+
 function off() {
     document.getElementById("overlay").style.display = "none";
 }
+
+// Event listener for the "Sign Up" link in the top bar
+document.querySelector('.signup-link').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent the default link behavior
+    document.getElementById('signupPopup').style.display = 'block';
+    on();
+});
